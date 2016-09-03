6f05724ab90d9ade86a1f5d52b67d2f9dea377eb
Andrew McPherson <andrewmcp333@gmail.com>
2016-09-03 11:13:23 -0700

    Wrote a very very messy trail effect.

--- source/scripts/models/Game.js
+++ source/scripts/models/Game.js
@@ -4,6 +4,8 @@ class Game {
 
         this.thug = new Thug()
 
+        this.glitches = {}
+
         this.time = 0
     }
     update(delta) {
@@ -28,9 +30,39 @@ class Game {
 
         this.player.update(delta)
         this.thug.update(delta)
-        
+
+        if(delta.glitchtime.inFrames < 0.5) {
+            var glitch = {
+                position: {
+                    x: Math.floor(this.player.position.x),
+                    y: Math.floor(this.player.position.y),
+                },
+                width: this.player.width,
+                height: this.player.height,
+                color: "rgba(255, 255, 255, 0.2)",
+                anchor: {
+                    x: this.player.anchor.x,
+                    y: this.player.anchor.y,
+                },
+                time: 5
+            }
+            var key = glitch.position.x + "x" + glitch.position.y
+            this.glitches[key] = glitch
+        }
+
         render.clear()
-        render.render(game.thug)
-        render.render(game.player)
+        render.render(this.thug)
+        
+        for(var key in this.glitches) {
+            var glitch = this.glitches[key]
+            glitch.time -= delta.glitchtime.inFrames
+            if(glitch.time <= 0) {
+                delete this.glitches[key]
+            } else {
+                render.render(glitch)
+            }
+        }
+
+        render.render(this.player)
     }
 }
